a074ac1ef7d3ae5d3e3330e0a31dfca92720f230
BBaimtoff1 <120623006+BBaimtoff1@users.noreply.github.com>
2023-01-07 12:42:33 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Printable[] printables = {createObjects("Apple"), createObjects("Samsung"),
+         createObjects("Redmi")};
+        for (Printable printable:printables) {
+            printable.print();
+
+        }
+
+        }
+
+
+
+    public static Printable createObjects (String className){
+        switch (className){
+            case "Apple":
+                return new Apple("суперЧип", "реалистичнаяКамера",
+                        "яркийДисплей", "Mini");
+            case "Samsung":
+                return new Samsung(167, 1080, 7000, 60);
+
+            case "Redmi":
+                return new Redmi("big", "powerCamera", 6, "Blue");
+        }
+
+        return null;
     }
 }
