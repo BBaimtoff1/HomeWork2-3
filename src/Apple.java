675a46b4ef9b20c5097e738a83be39667d5eac01
BBaimtoff1 <120623006+BBaimtoff1@users.noreply.github.com>
2023-01-07 12:41:52 +0300

    Update Apple.java

--- src/Apple.java
+++ src/Apple.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Apple {
+public class  Apple extends Mobile {
+    private String powerChip;
+
+    private String powerCamera;
+
+    private String brightDisplay;
+
+    private String size;
+
+
+    public Apple(String powerChip, String powerCamera, String brightDisplay, String size) {
+        this.powerChip = powerChip;
+        this.powerCamera = powerCamera;
+        this.brightDisplay = brightDisplay;
+        this.size = size;
+    }
+
+    @Override
+    public void print() {
+        System.out.println("powerChip" + powerChip + "\npowerCamera" + powerCamera + "\nbrightDisplay" +
+                brightDisplay + "\nsize" + size);
+        System.out.println("------------");
+
+
+    }
+
 }
