8c836062f8d3c50150a622650334162849c2cd86
BBaimtoff1 <120623006+BBaimtoff1@users.noreply.github.com>
2023-01-07 12:44:20 +0300

    Update Samsung.java

--- src/Samsung.java
+++ src/Samsung.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Samsung {
+public class Samsung extends Mobile{
+private int Gram;
+private int Pixel;
+private int Battery;
+
+private int FrameFrequency;
+
+    public Samsung( int Gram, int pixel, int battery, int frameFrequency) {
+        this.Gram = Gram;
+        Pixel = pixel;
+        Battery = battery;
+        FrameFrequency = frameFrequency;
+    }
+
+
+
+
+    @Override
+    public void print() {
+        System.out.println("Gram" + Gram + "\nPixel" + Pixel + "\nBattery" + Battery + "\nFrameFrequency" +
+                FrameFrequency);
+        System.out.println("--------------");
+    }
 }
