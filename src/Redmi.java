3bd44442ebb74437c6bced9e906bb8c58db9e97a
BBaimtoff1 <120623006+BBaimtoff1@users.noreply.github.com>
2023-01-07 12:43:54 +0300

    Update Redmi.java

--- src/Redmi.java
+++ src/Redmi.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Redmi {
+public class Redmi extends Mobile{
+    private String Display;
+    private String Camera;
+    private int GHZ;
+    private String Color;
+
+    public Redmi (String display, String camera, int GHZ, String color) {
+        Display = display;
+        Camera = camera;
+        this.GHZ = GHZ;
+        Color = color;
+    }
+
+
+    @Override
+    public void print() {
+        System.out.println("Display" + Display + "\nCamera" + Camera + "\nGHZ" + GHZ + "\nColor" + Color);
+
+
+    }
 }
